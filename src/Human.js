76f74085de6708ba0d88ad962c0439f150558a77
Alexander Volkov <agvolkov4@yandex.ru>
2019-03-24 22:46:39 +0300

    second step :)

--- src/Human.js
+++ src/Human.js
@@ -22,95 +22,88 @@ class Human extends Component {
         };
 
         setTimeout(() => {
-            this.startStep(20, this.continueStep(40));
+            this.startStep(20, () => {
+                this.continueStep(20);
+            });
         }, 1000);
     }
 
     startStep = (distance, callback) => {
         this.setState({
             animated: true,
-            leftFootOffset: distance + 3,
-            bodyOffset: -distance * 0.4,
-            rightFootOffset: -distance * 0.8,
+            y: this.state.y - distance,
+            leftFootOffset: distance,
             leftBoatOffset: distance,
+            rightFootOffset: -distance - this.state.boatHeight * 0.45,
+            rightBoatOffset: -distance,
         },
         () => {
             const that = this;
             setTimeout(() => {
-                that.setState({
-                    animated: false,
-                    y: that.state.y - distance * 0.4,
-                    leftFootOffset: distance + 3 - distance * 0.4,
-                    bodyOffset: 0,
-                    rightFootOffset: distance * 0.8 + distance * 0.4,
-                    leftBoatOffset: distance - distance * 0.4,
-                    rightBoatOffset: 1 - distance * 0.4,
-                });
-                setTimeout(() => {
-                    if (typeof callback === 'function') {
-                        callback();
-                    }
-                }, 500);
-            }, 500);
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            }, 300);
         });
     };
 
     continueStep = (distance, callback) => {
         this.setState({
             animated: true,
-            leftFootOffset: distance + 3,
-            bodyOffset: -distance * 0.4,
-            rightFootOffset: -distance * 0.8,
-            leftBoatOffset: distance,
+            y: this.state.y - distance,
+            leftFootOffset: -distance,
+            leftBoatOffset: -distance + this.state.boatHeight * 0.45,
+            rightFootOffset: distance + 3,
+            rightBoatOffset: distance,
         },
         () => {
             const that = this;
             setTimeout(() => {
-                that.setState({
-                    animated: false,
-                    y: that.state.y - distance * 0.4,
-                    leftFootOffset: distance + 3 - distance * 0.4,
-                    bodyOffset: 0,
-                    rightFootOffset: distance * 0.8 + distance * 0.4,
-                    leftBoatOffset: distance - distance * 0.4,
-                    rightBoatOffset: 1 - distance * 0.4,
-                });
-                setTimeout(() => {
-                    if (typeof callback === 'function') {
-                        callback();
-                    }
-                }, 500);
+                // that.setState({
+                //     animated: false,
+                //     y: that.state.y - distance * 0.5,
+                //     rightFootOffset: distance + 3 + distance * 0.5,
+                //     rightBoatOffset: distance - distance,
+                //     bodyOffset: 0,
+                //     leftFootOffset: -50,
+                //     leftBoatOffset: -distance * 0.5,
+                // });
+                // setTimeout(() => {
+                //     if (typeof callback === 'function') {
+                //         callback();
+                //     }
+                // }, 500);
             }, 500);
         });
     };
 
     render() {
         return (
-            <div className={this.state.animated ? 'human' : 'human non-transition'}
+            <div className={this.state.animated ? 'Human' : 'Human non-transition'}
                 style={{
-                    top: this.state.y + 'pt',
-                    left: this.state.x + 'pt'
+                    top: this.state.y + 'px',
+                    left: this.state.x + 'px'
                 }}>
                 <div className="boats">
                     <div className="left boat"
                         style={{top: this.state.bodyHeight / 2 - this.state.boatHeight / 2
-                                        - this.state.leftBoatOffset + 'pt'}}/>
+                                        - this.state.leftBoatOffset + 'px'}}/>
                     <div className="right boat"
                          style={{top: this.state.bodyHeight / 2 - this.state.boatHeight / 2
-                                        - this.state.rightBoatOffset + 'pt'}}/>
+                                        - this.state.rightBoatOffset + 'px'}}/>
                 </div>
                 <div className="foots">
                     <div className="left foot"
-                        style={{height: this.state.leftFootOffset + 'pt',
-                                top: this.state.bodyHeight / 2 - this.state.leftFootOffset + 'pt'}}/>
+                        style={{height: this.state.leftFootOffset * (1 - 2 * (this.state.leftFootOffset < 0)) + 'px',
+                                top: this.state.bodyHeight / 2 - this.state.leftFootOffset * (this.state.leftFootOffset > 0) + 'px'}}/>
                     <div className="right foot"
-                         style={{height: -this.state.rightFootOffset + 'pt',
-                             top: this.state.bodyHeight / 2 + this.state.bodyOffset + 'pt'}}/>
+                         style={{height: this.state.rightFootOffset * (1 - 2 * (this.state.rightFootOffset < 0)) + 'px',
+                                top: this.state.bodyHeight / 2 - this.state.rightFootOffset * (this.state.rightFootOffset > 0) + 'px'}}/>
                 </div>
                 <div className="body"
-                    style={{top: this.state.bodyOffset + 'pt'}}/>
+                    style={{top: this.state.bodyOffset + 'px'}}/>
                 <div className="head"
-                     style={{top: this.state.bodyOffset - 3.8 + 'pt'}}/>
+                     style={{top: this.state.bodyOffset - 3.8 + 'px'}}/>
             </div>
         );
     }
