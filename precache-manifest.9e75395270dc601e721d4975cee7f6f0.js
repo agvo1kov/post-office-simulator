1df4942a4199bd80682b665df4e6ca154a7740b4
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-01 07:23:05 +0300

    Updates

---
+++ precache-manifest.9e75395270dc601e721d4975cee7f6f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2029de48b0b16b066b8f9f7c40172e29",
+    "url": "/post-office-simulator/static/media/office-floor.2029de48.jpg"
+  },
+  {
+    "revision": "b9cf333f82aa5822a296",
+    "url": "/post-office-simulator/static/css/main.c41b2583.chunk.css"
+  },
+  {
+    "revision": "75d37ce01b2845e0fb42",
+    "url": "/post-office-simulator/static/js/runtime~main.236d829a.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/post-office-simulator/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "498c93fa1b9e84aa79a99c30a2a59156",
+    "url": "/post-office-simulator/static/media/wall.498c93fa.jpg"
+  },
+  {
+    "revision": "01bfa6926cdfddf55b9605252d6e7efe",
+    "url": "/post-office-simulator/static/media/street-floor.01bfa692.jpg"
+  },
+  {
+    "revision": "b9cf333f82aa5822a296",
+    "url": "/post-office-simulator/static/js/main.cc92ec76.chunk.js"
+  },
+  {
+    "revision": "394f141d84051fb82ea30be2926f88d4",
+    "url": "/post-office-simulator/static/media/atm.394f141d.jpg"
+  },
+  {
+    "revision": "79d953a67cc1508da0a79ec3bacabdc0",
+    "url": "/post-office-simulator/static/media/window.79d953a6.jpg"
+  },
+  {
+    "revision": "33a49eb0d890eceacc5e8d3ac7aed053",
+    "url": "/post-office-simulator/static/media/stack.33a49eb0.png"
+  },
+  {
+    "revision": "31a6cd7419ffecbc3e8c535d6a16dd01",
+    "url": "/post-office-simulator/static/media/empty-stack.31a6cd74.png"
+  },
+  {
+    "revision": "2b4a0feab479a6802a55d8d7b5aaf4e9",
+    "url": "/post-office-simulator/index.html"
+  }
+];
